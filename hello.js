02e268e3db59c8f445cb1453a12fd5d9c5433bb7
Thea Ganden <wtp9998@hotmail.com>
2020-03-14 02:02:08 +0000

    The difference between console.log and return

--- hello.js
+++ hello.js
@@ -1,4 +1,15 @@
 const sayHello = function (name) {
 	console.log("Hello, " + name);
-}
+	}
 sayHello("Thea");
+
+const sayHelloToConsole = function (name) {
+	console.log("Hello, " + name);
+	}
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+	return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
